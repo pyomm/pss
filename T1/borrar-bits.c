57a31495e7fa8812aed52a85dc69a25157bbf3ae
pyomm <victoria.labarca.s@gmail.com>
2022-04-01 16:44:26 -0300

    es la que entregue

--- T1/borrar-bits.c
+++ T1/borrar-bits.c
@@ -4,15 +4,15 @@
 #include "borrar-bits.h"
 
 uint borrar_bits(uint x, uint pat, int len) {
-  uint r;  //variable auxiliar cualquiera
+  uint r = 0;  //variable auxiliar cualquiera
   int lo = len;  //variable auxiliar = len
   uint a = sizeof(uint)<<3; //largo de un uint definido como a, para ahorrar escritura adelante
   while (lo <= a){  //while para recorrer todos los bits de x
-    uint r2 = (((x^pat)<<(a-lo))>>(a-len));  //x^pat ve si pat esta en cierto grupo de bits de x y con <<, >> se ignoran bits no evaluados en este ciclo
-    if (r2 == 0){  //si la seccion evaluada resulta compuesta solo de 0
+    uint r2 = (((x^pat)<<(a-lo))>>(a-len));  //x^pat ve si pat esta en cierto grupo de bits de x, usando <<, >> se ignoran bits no evaluados en este ciclo, igualandolos a 0
+    if (r2 == 0){  //si la seccion evaluada era igual a pat
       r = r|(x&pat); //r entrega 1 en las posiciones donde hay 1s del patron a cambiar por 0
-      pat <<= (len-1); //len-1 porque se omite <<1 que se hace fuera del if
-      lo += (len-1);
+      pat <<= (len-1); //se traslada pat tal que no revise nuevamente la seccion de x modificada
+      lo += (len-1);  //lo aumenta equivalente a los espacios del shift para pat ((len-1) porque se agrega +1 siempre fuera del if)
     }
     pat <<= 1;  //mueve pat a la izquierda para poder comparar todas las partes de x
     lo++; //avanza los bits de x revisados
