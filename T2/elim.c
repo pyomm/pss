be216c5fe88a45e5e701d89c035fe04a727002ab
pyomm <victoria.labarca.s@gmail.com>
2022-04-08 23:36:51 -0400

    make run-san no listo

--- T2/elim.c
+++ T2/elim.c
@@ -19,7 +19,7 @@ void eliminar_espacios(char *s) {
         e=1;
       }
     }
-    else if (e==1){
+    else{ //if e==1
       if(*g!=' '){
         *h=*g;
         h++;
@@ -38,42 +38,42 @@ char *eliminacion_espacios(const char *s) {
   char *a=(char *) s; //puntero lector
   int l=strlen(s)+1;
   int e=0;  //e=1 si el puntero lector quedo en un espacio, e=0 si quedo en un caracter != ' '
-  while (*a !=0){
-    if (e==1){
-      if (*a == ' '){
+  while (*a!=0){
+    if (e==0){
+      if (*a==' '){
+        e=1;
+      }
+    }
+    else{ //if e==1
+      if (*a==' '){
         l--;
       }
       else{
         e=0;
       }
     }
-    if (e==0){
-      if (*a == ' '){
-        e=1;
-      }
-    }
+
     a++;
   }
   char *p=malloc(l+1);
   char *r=p;
   e=0;  //e=1 si el puntero lector (s) quedo en un espacio, e=0 si quedo en un caracter != ' '
-  while (*s !=0){
-    if (e==1 && *s != ' '){
-      //if (*b == ' ') no se necesita especificar porque no hace nada, solo revisa el siguiente caracter en s, o sea s++
+  while (*s!=0){
+    if (e==1){
+      if (*s!=' '){
+        *r=*s;
+        r++;
+        e=0;
+      }
+    } //si e==1 y s== ' ' no se necesita especificar porque no hace nada, solo revisa el siguiente caracter en s, o sea s++
+    else{ //if e==0
       *r=*s;
       r++;
-      e=0;
-    }
-    else if (e==0){
-      *r=*s;
-      r++;
-      if (*s == ' '){
+      if (*s==' '){
         e=1;
       }
     }
     s++;
   }
-  r++;
-  *r=0;
   return p;
 }
