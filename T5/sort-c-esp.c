001841bbd56c428c07cf9125f1c299ae704310e1
pyomm <victoria.labarca.s@gmail.com>
2022-05-21 18:20:28 -0400

    codigo c listo

--- T5/sort-c-esp.c
+++ T5/sort-c-esp.c
@@ -1,14 +1,34 @@
 #include <string.h>
 
 void sort(char **noms, int n) {
-  char **ult= &noms[n-1];
-  char **p= noms;
-  while (p<ult) {
+  char **ult= &noms[n-1];   //ultimo string del arreglo
+  char **p= noms;   //primer string del arreglo, varia
+  while (p<ult) {   //mientras no se llegue al ultimo string
+
     ////////////////////////////////////////////
     // Comienza el codigo que Ud. debe modificar
     ////////////////////////////////////////////
- 
-    int rc= strcmp(p[0], p[1]);
+
+    char *s1=p[0];
+    char *s2=p[1];
+    int rc=0;
+    while (rc==0){
+        if (*s1==' '){
+            while (*s2!=' ') {
+                s2++;
+                if (*s2 == 0) { //se terminó de leer p[1] habiendo espacios no igualados en p[0]
+                    rc=1;
+                }
+            }
+            if(*s2==' '){
+                s2++;
+            }
+        }
+        s1++;
+        if(*s1==0){   //se terminó de leer p[0] sin encontrarse mas espacios que en p[1]
+            rc=-1;
+        }
+    }
 
     ////////////////////////////////////////////
     // Fin del codigo que Ud. debe modificar
